23613c2e2fb0a1886e99246a6ac643d18c57a19d
Dandy <mochamad.dandy@gmail.com>
2019-01-04 15:21:28 +0700

    init commit for spring-boot-actuator

--- src/main/java/com/example/mitrais/ProjectWebSimple/Application.java
+++ src/main/java/com/example/mitrais/ProjectWebSimple/Application.java
@@ -2,6 +2,7 @@ package com.example.mitrais.ProjectWebSimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
