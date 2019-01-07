c491e62bc208348cac8fde3e0c710840cbda782d
Dandy <mochamad.dandy@gmail.com>
2019-01-07 10:41:35 +0700

    adding web security login mechanisms - all requests are redirected to login page

--- src/main/java/com/example/mitrais/ProjectWebSimple/Application.java
+++ src/main/java/com/example/mitrais/ProjectWebSimple/Application.java
@@ -2,7 +2,6 @@ package com.example.mitrais.ProjectWebSimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
